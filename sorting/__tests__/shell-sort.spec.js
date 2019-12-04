2b90e9d6963fc8b454d2984792e96ca2bf16aaca
arthur <arthur.osipenko@gmail.com>
2019-12-04 23:36:37 +0200

    add shell sort

---
+++ sorting/__tests__/shell-sort.spec.js
@@ -0,0 +1,25 @@
+import { shellSort } from "../shell-sort";
+
+describe('Shell sort', () => {
+    describe('shellSort function', () => {
+        test('should sort array of number in ascending order',() => {
+            expect(shellSort([2, 4, 6, 1])).toStrictEqual([1, 2, 4, 6]);
+            expect(shellSort([9, 3, 1, 5])).toStrictEqual([1, 3, 5, 9]);
+        });
+        test('should not mutate original array',() => {
+            let array = [2, 4, 6, 1];
+            shellSort(array);
+            expect(array).toStrictEqual([2, 4, 6, 1]);
+        });
+        test('should not mutate original array even if it stay the same',() => {
+            let array = [1, 2, 4, 6];
+            let sortedArray = shellSort(array);
+            expect(array).not.toBe(sortedArray);
+        })
+        test('should not mutate original array even if it contain one item',() => {
+            let array = [1];
+            let sortedArray = shellSort(array);
+            expect(array).not.toBe(sortedArray);
+        })
+    });
+});
---
+++ sorting/shell-sort.js
@@ -0,0 +1,33 @@
+// Time complexity: O(n*(log(n))^2)
+// Memory: 1
+export function shellSort([...array]) {
+    // Define a gap distance.
+    let gap = Math.floor(array.length / 2);
+
+    // Until gap is bigger then zero do elements comparisons and swaps.
+    while (gap > 0) {
+        // Go and compare all distant element pairs.
+        for (let i = 0, length = array.length; i < (length - gap); i += 1) {
+            let currentIndex = i;
+            let gapShiftedIndex = i + gap;
+
+            while (currentIndex >= 0) {
+                // Compare and swap array elements if needed.
+                if (array[gapShiftedIndex] < array[currentIndex]) {
+                    const tmp = array[currentIndex];
+                    array[currentIndex] = array[gapShiftedIndex];
+                    array[gapShiftedIndex] = tmp;
+                }
+
+                gapShiftedIndex = currentIndex;
+                currentIndex -= gap;
+            }
+        }
+
+        // Shrink the gap.
+        gap = Math.floor(gap / 2);
+    }
+
+    // Return sorted copy of an original array.
+    return array;
+}
