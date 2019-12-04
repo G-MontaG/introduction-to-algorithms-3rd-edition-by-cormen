bc3a027824037addfdc5077be412164076250da6
arthur <arthur.osipenko@gmail.com>
2019-12-04 23:51:03 +0200

    fix formating

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -4,33 +4,33 @@ export function quickSort([...array]) {
     // If array has less than or equal to one elements then it is already sorted.
     if (array.length <= 1) {
         return array;
-      }
-  
-      // Init left and right arrays.
-      const leftArray = [];
-      const rightArray = [];
-  
-      // Take the first element of array as a pivot.
-      const pivotElement = array.shift();
-      const centerArray = [pivotElement];
-  
-      // Split all array elements between left, center and right arrays.
-      while (array.length) {
+    }
+
+    // Init left and right arrays.
+    const leftArray = [];
+    const rightArray = [];
+
+    // Take the first element of array as a pivot.
+    const pivotElement = array.shift();
+    const centerArray = [pivotElement];
+
+    // Split all array elements between left, center and right arrays.
+    while (array.length) {
         const currentElement = array.shift();
-  
+
         if (currentElement === pivotElement) {
-          centerArray.push(currentElement);
+            centerArray.push(currentElement);
         } else if (currentElement < pivotElement) {
-          leftArray.push(currentElement);
+            leftArray.push(currentElement);
         } else {
-          rightArray.push(currentElement);
+            rightArray.push(currentElement);
         }
-      }
-  
-      // Sort left and right arrays.
-      const leftArraySorted = quickSort(leftArray);
-      const rightArraySorted = quickSort(rightArray);
-  
-      // Let's now join sorted left array with center array and with sorted right array.
-      return leftArraySorted.concat(centerArray, rightArraySorted);
+    }
+
+    // Sort left and right arrays.
+    const leftArraySorted = quickSort(leftArray);
+    const rightArraySorted = quickSort(rightArray);
+
+    // Let's now join sorted left array with center array and with sorted right array.
+    return leftArraySorted.concat(centerArray, rightArraySorted);
 }
