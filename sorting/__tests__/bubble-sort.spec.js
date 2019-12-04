d293e87c0ca7ae11f4bfb9c4d2adfa29af3ecc86
arthur <arthur.osipenko@gmail.com>
2019-12-04 23:11:01 +0200

    add bubble sort

---
+++ sorting/__tests__/bubble-sort.spec.js
@@ -0,0 +1,25 @@
+import { bubbleSort } from "../bubble-sort";
+
+describe('Bubble sort', () => {
+    describe('bubbleSort function', () => {
+        test('should sort array of number in ascending order',() => {
+            expect(bubbleSort([2, 4, 6, 1])).toStrictEqual([1, 2, 4, 6]);
+            expect(bubbleSort([9, 3, 1, 5])).toStrictEqual([1, 3, 5, 9]);
+        });
+        test('should not mutate original array',() => {
+            let array = [2, 4, 6, 1];
+            bubbleSort(array);
+            expect(array).toStrictEqual([2, 4, 6, 1]);
+        });
+        test('should not mutate original array even if it stay the same',() => {
+            let array = [1, 2, 4, 6];
+            let sortedArray = bubbleSort(array);
+            expect(array).not.toBe(sortedArray);
+        })
+        test('should not mutate original array even if it contain one item',() => {
+            let array = [1];
+            let sortedArray = bubbleSort(array);
+            expect(array).not.toBe(sortedArray);
+        })
+    });
+});
